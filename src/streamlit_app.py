ea20000baea5ea9500ac68e84616fc15ccba71cb
Ryan de la Garza <ryan.delagarza@imaginelearning.com>
2025-09-18 15:31:55 -0500

    Updated browser tab title

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -11,7 +11,7 @@ from datetime import datetime
 
 # Configure the page
 st.set_page_config(
-    page_title="Errata Viewer",
+    page_title="Improvements Viewer",
     page_icon="📚",
     layout="wide",
     initial_sidebar_state="expanded"
